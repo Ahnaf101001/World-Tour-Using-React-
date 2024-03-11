a8edafcc895c0ccad8be63bb89d942b41d958165
Ahnaf Khan <ahnaf10100@gmail.com>
2024-03-11 18:50:21 +0600

    My first commit

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountry}) => {
     console.log(country);
     const {name, flags, capital, population, area, cca3} = country;
     const [ visited, setVisited] = useState(false);
     const handleVisited = () =>{
         setVisited(!visited);
     }
+
+    // const passWithParams = () => handleVisitedCountry(country);
+
     return (
-        <div className={`country ${visited && 'visited'}`}>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
             <h3>Name: {name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Capital: {capital}</p>
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p>Code: {cca3}</p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark as Visited</button><br /><br />
             <button onClick={handleVisited}>{visited? "Visited" : "Not Visited"}</button>
             {visited ? "I have visited the country" : "I want to vivsit"}
         </div>
